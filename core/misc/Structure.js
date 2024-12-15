1ee9d7f11d8157d0ad3c9e7f1d9f357c20353545
Maksim Ruletov <88242162+maksim-ruletov@users.noreply.github.com>
2024-12-15 23:42:08 +0300

    dev-1.1.8-a15212024
    - Fixed bug with folder names

--- core/misc/Structure.js
+++ core/misc/Structure.js
@@ -2,12 +2,12 @@ const os = require('os');
 
 const FOLDERS_PATH = {
     'company': `${os.homedir()}/AppData/Roaming/1Error Studios`,
-    'app': `${os.homedir()}/AppData/Roaming/1Error Studios/Crystall`,
-    'plugins': `${os.homedir()}/AppData/Roaming/1Error Studios/Crystall/plugins`
+    'app': `${os.homedir()}/AppData/Roaming/1Error Studios/Cristall`,
+    'plugins': `${os.homedir()}/AppData/Roaming/1Error Studios/Cristall/plugins`
 }
 
 const FILES_PATH = {
-    'log_file': `${os.homedir()}/AppData/Roaming/1Error Studios/Crystall/log.json`
+    'log_file': `${os.homedir()}/AppData/Roaming/1Error Studios/Cristall/log.json`
 }
 
 exports.FOLDERS_PATH = FOLDERS_PATH;
