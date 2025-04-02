cb268923e071b5a411194cec84df542bef993214
Maksim Ruletov <88242162+maksim-ruletov@users.noreply.github.com>
2025-04-02 11:44:40 +0300

    dev-1.18.56a [4/2/2025]
    - Basic implementation of file loader

--- web/js/web.editor/Editor.js
+++ web/js/web.editor/Editor.js
@@ -7,6 +7,8 @@ class Editor {
     }
 
     Initialize(fileContent) {
+        this.title = fileContent.name;
+
         window.electronAPI.invoke('files:load-file', {
             id: loader.GetActiveWorkspace(),
             filename: fileContent.filename
@@ -24,10 +26,53 @@ class Editor {
     }
 
     LoadMD(content) {
-        console.log(marked.parse(content));
+        let parsedMarkdown = content.split('\n');
+        parsedMarkdown = this.CleanUpContent(parsedMarkdown);
+
+        TakeNote('{WEB}.Editor.LoadMD(content)', `INFO: parsed markdown: ${JSON.stringify(parsedMarkdown)}`);
+
+        parsedMarkdown.forEach(item => {
+            let element = marked.parse(item);
+
+            console.log(element);
+
+            this.elements.push(element);
+        });
+
+        this.ImplementElements();
     }
 
     LoadJSON(content) {
+        
+    }
 
+    ImplementElements() {
+        TakeNote('{WEB}.Editor.ImplementElements()', `INFO: created ${this.elements.length} elements`);
+
+        document.querySelector('[interface-role-id="viewport"]').innerHTML += `<h1 class="viewport-title">${this.title.length > 0 ? this.title : 'Unnamed'}</h1>`
+
+        this.elements.forEach(element => {
+            document.querySelector('[interface-role-id="viewport"]').innerHTML += element;
+        });
+
+        document.querySelector('[interface-role-id="viewport"]').childNodes.forEach((item, index) => {
+            if (item.tagName) {
+                if (item.tagName === 'P') {
+                    item.classList.add('viewport-text');
+                }
+            }
+        })
+    }
+
+    CleanUpContent(content) {
+        let result = [];
+
+        for (let i = 0; i < content.length; i++) {
+            if (!['\r', '\n'].includes(content[i])) {
+                result.push(content[i]);
+            }
+        }
+
+        return result;
     }
 }
