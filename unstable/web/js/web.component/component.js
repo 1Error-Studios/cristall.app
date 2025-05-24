5eedede26c039f49f5e2dcfc655bc3ba7ed6c214
Maksim Ruletov <88242162+maksim-ruletov@users.noreply.github.com>
2025-05-24 18:38:02 +0300

    unstable-1.26.1
    - Add basic structure for Renderer and Components modules.

---
+++ unstable/web/js/web.component/component.js
@@ -0,0 +1,91 @@
+class Component {
+    constructor(componentIndex, componentName) {
+        this.componentIndex = componentIndex;
+        this.componentName = componentName ?? `unnamed.${componentIndex}`;
+
+        this.styleMap = [];
+        this.styleIndexes = [];
+
+        this.events = [];
+
+        this.innerComponents = [];
+
+        this.utilities = [];
+    }
+
+    GetComponentIndex() {
+
+    }
+
+    GetComponentName() {
+
+    }
+
+    LoadStyleMap() {
+
+    }
+
+    LoadStyleIndexes() {
+
+    }
+
+    DeleteStyleMapEntry() {
+
+    }
+
+    DeleteStyleIndexesEntry() {
+
+    }
+
+    ChangeStyleMapEntry() {
+
+    }
+
+    ChangeStyleIndexesEntry() {
+
+    }
+
+    GetStyleMapEntry() {
+
+    }
+
+    GetStyleIndexesEntry() {
+
+    }
+
+    SetupEvent() {
+
+    }
+
+    RemoveEvent() {
+
+    }
+
+    AppendChildComponent() {
+
+    }
+
+    GetChildComponent() {
+
+    }
+
+    EditChildComponent() {
+
+    }
+
+    RemoveChildComponent() {
+
+    }
+
+    AppendUtility() {
+
+    }
+
+    CallUtility() {
+
+    }
+
+    RemoveUtility() {
+        
+    }
+}
